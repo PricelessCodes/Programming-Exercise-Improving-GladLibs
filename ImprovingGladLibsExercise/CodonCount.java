d5347155bed7d98c4b5348f40202cfac9ac2af7a
Yahia Mahmoud Abdelrahman Bassyouny <66659379+PricelessCodes@users.noreply.github.com>
2022-10-08 02:56:51 +0200

    Add files via upload

---
+++ ImprovingGladLibsExercise/CodonCount.java
@@ -0,0 +1,80 @@
+
+/**
+ * Write a description of CodonCount here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import java.util.*;
+
+public class CodonCount {
+    
+    private HashMap<String,Integer> DNACodons;
+    
+    public CodonCount()
+    {
+        DNACodons = new HashMap<String,Integer>();
+    }
+
+    private void buildCodonMap(int start, String dna)
+    {
+        DNACodons.clear();
+        
+        for (int i = start; i < dna.length() - 2; i += 3)
+        {
+            String codon = dna.substring(i, i + 3);
+            if (!DNACodons.containsKey(codon))
+            {
+                DNACodons.put(codon,1);
+            }
+            else
+            {
+                DNACodons.put(codon, DNACodons.get(codon) + 1);
+            }
+        }
+    }
+    
+    private String getMostCommonCodon()
+    {
+        int max = -1;
+        String maxCodon = "";
+        for (String codon : DNACodons.keySet())
+        {
+            if (DNACodons.get(codon) > max)
+            {
+                max = DNACodons.get(codon);
+                maxCodon = codon;
+            }
+        }
+        return maxCodon;
+    }
+    
+    private void printCodonCounts(int start, int end)
+    {
+        for (String codon : DNACodons.keySet())
+        {
+            int value = DNACodons.get(codon);
+            if (value >= start && value <= end)
+            {
+                System.out.println("total count: " + value + " of codon: " + codon);
+            }
+        }
+    }
+    
+    public void tester()
+    {
+        FileResource fr = new FileResource();
+        String dna = fr.asString().toUpperCase().trim();
+        System.out.println("the dna is: " + dna);
+        buildCodonMap(0, dna);
+        System.out.println("the total number of unique codons is: " + DNACodons.size());
+        
+        String mostCommonCodon = getMostCommonCodon();
+        System.out.println("the most common codon is: " + mostCommonCodon + " and its count is: " + DNACodons.get(mostCommonCodon));
+        
+        int start = 1, end = 3;
+        System.out.println("the codons and their number of occurrences for those codons whose number of occurrences are between (" + start + " and " + end + ") inclusive: ");
+        printCodonCounts(start, end);
+    }
+}
---
+++ ImprovingGladLibsExercise/WordsInFiles.java
@@ -0,0 +1,133 @@
+
+/**
+ * Write a description of WordsInFiles here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import java.util.*;
+import java.io.*;
+
+public class WordsInFiles {
+
+    private HashMap<String,ArrayList<String>> words;
+    
+    public WordsInFiles()
+    {
+        words = new HashMap<String,ArrayList<String>>();
+    }
+    
+    private void addWordsFromFile(File f)
+    {
+        FileResource fr = new FileResource(f);
+        
+        for (String word: fr.words())
+        {
+            word = word.toLowerCase();
+            
+            if (!words.containsKey(word))
+            {
+                ArrayList<String> files = new ArrayList<String>();
+                files.add(f.getName());
+                words.put(word, files);
+            }
+            else
+            {
+                ArrayList<String> files = words.get(word);
+                if (!files.contains(f.getName()))
+                {
+                    files.add(f.getName());
+                    words.put(word, files);
+                }
+            }
+        }
+    }
+    
+    private void buildWordFileMap()
+    {
+        words.clear();
+        
+        DirectoryResource dr = new DirectoryResource();
+        // iterate over files
+        for (File f : dr.selectedFiles())
+        {
+            addWordsFromFile(f);
+        }
+    }
+    
+    private int maxNumber()
+    {
+        int max = -1;
+        String maxWord = "";
+        for (String word : words.keySet())
+        {
+            int length = words.get(word).size();
+            if (length > max)
+            {
+                max = length;
+                maxWord = word;
+            }
+        }
+        System.out.println("the most common word in files is: (" + maxWord + ") and it is occuranced inside (" + max + ") files");
+        return max;
+    }
+    
+    private ArrayList<String> wordsInNumFiles(int number)
+    {
+        ArrayList<String> wordsList = new ArrayList<String>();
+        
+        for (String word : words.keySet())
+        {
+            int length = words.get(word).size();
+            if (length == number)
+            {
+                wordsList.add(word);
+            }
+        }
+        return wordsList;
+    }
+    
+    private void printFilesIn(String word)
+    {
+        String print = word + " exists in (";
+        ArrayList<String> files = words.get(word);
+        
+        for (int i = 0; i < files.size(); i++)
+        {
+            String file = files.get(i);
+            print += (file + ", ");
+        }
+        print += ")";
+        System.out.println(print);
+    }
+    
+    private void printAll()
+    {
+        System.out.println("Print all inside hashmap");
+        for (String word : words.keySet())
+        {
+            printFilesIn(word);
+        }
+    }
+    
+    public void tester()
+    {
+        buildWordFileMap();
+        
+        printAll();
+        System.out.println("\n");
+        
+        int occurrance = maxNumber();
+        System.out.println("\n");
+        
+        int number = 2;
+        ArrayList<String> wordList = wordsInNumFiles(number);
+        System.out.println("Words that occurred in (" + number + ") files");
+        for (int i = 0; i < wordList.size(); i++)
+        {
+            printFilesIn(wordList.get(i));
+        }
+        System.out.println("\n");
+    }
+}
